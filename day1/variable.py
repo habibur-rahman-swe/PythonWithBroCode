5f31bdf51cbfbf759574da7e285dc6703e04063a
Habibur Rahman <habibur0199mhr@gmail.com>
2023-08-11 21:29:57 +0600

    4 types of variable

--- day1/variable.py
+++ day1/variable.py
@@ -1,3 +1,19 @@
-name = "Bro"
-print(name)
-print(type(name))
+# # first_name = "Bro"
+# # last_name = "Code"
+# # full_name = first_name + " " + last_name
+
+# # print("Hello " + full_name)
+
+# age = 31
+# age = age + 1
+# print(age)
+# print(type(age))
+
+# height = 250.5
+# print("your height is " + str(height) + "cm")
+# print(type(height))
+
+human = False
+print("Are you a human: " + str(human))
+print(type(human))
+
