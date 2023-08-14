172120f71055c53a19b8fb4d04809f4461e5c221
Habibur Rahman <habibur0199mhr@gmail.com>
2023-08-14 12:54:57 +0600

    loop control

--- day1/variable.py
+++ day1/variable.py
@@ -171,3 +171,23 @@
 #     for j in range(columns):
 #         print(symbol, end=" ")
 #     print()
+
+# loop control statement
+
+# while True:
+#     name = input("Enter your name: ")
+#     if name != "":
+#         break;
+
+# phone_number = "123-456-8090"
+
+# for i in phone_number:
+#     if i == '-':
+#         continue
+#     print(i, end="")
+
+for i in range(1, 5):
+    if i == 4:
+        pass
+    else:
+        print(i)
