20fd4787cc63911329b2e8af29415d56f30e253f
Habibur Rahman <habibur0199mhr@gmail.com>
2023-08-14 07:15:40 +0600

    string slicing using slice object slice(start, stop, step)

--- day1/variable.py
+++ day1/variable.py
@@ -88,14 +88,25 @@
 
 # slicing = create a substring by extractiong elements from another string indexing[] or slice()
             # [start: stop: steep]
-name = "Bro code"
+# name = "Bro code"
 
-first_name = name[0:3]
-last_name = name[4:8]
-funky_name = name[0:8:2]
-reverse_name = name[::-1]
+# first_name = name[0:3]
+# last_name = name[4:8]
+# funky_name = name[0:8:2]
+# reverse_name = name[::-1]
 
-print(first_name)
-print(last_name)
-print(funky_name)
-print(reverse_name)
+# print(first_name)
+# print(last_name)
+# print(funky_name)
+# print(reverse_name)
+
+website1 = "https://google.com"
+website2 = "https://wiki.com"
+
+slice = slice(7, -4)
+
+website1 = website1[slice]
+website2 = website2[slice]
+
+print(website1)
+print(website2)
