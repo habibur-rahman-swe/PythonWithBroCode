48ce3e923447c06db4f72b73249e04515bbb4fb7
Habibur Rahman <habibur0199mhr@gmail.com>
2023-08-14 12:48:58 +0600

    nested loop

--- day1/variable.py
+++ day1/variable.py
@@ -161,3 +161,13 @@
 #     print(seconds)
 #     time.sleep(1)
 # print("Happy new year!")
+
+# nested loop 
+# rows = int(input("How many rows?: "))
+# columns = int(input("How many column? "))
+# symbol = input("Enter a symbol to use: ")
+
+# for i in range(rows):
+#     for j in range(columns):
+#         print(symbol, end=" ")
+#     print()
